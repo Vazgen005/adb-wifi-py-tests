fcf60fd6904bdcc16fd57de731f184cb088a5584
Vazgen005 <norvardyanvazgen@gmail.com>
2024-12-16 03:20:18 +0300

    Added paired device IP and port and colors

--- src/adb_wifi_py/main.py
+++ src/adb_wifi_py/main.py
@@ -10,15 +10,17 @@ https://github.com/jstasiak/python-zeroconf
 
 import logging
 import subprocess
+from random import randint
 from sys import argv, exit
 
-from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
+from colorama import Fore, just_fix_windows_console
 from qrcode import QRCode
+from zeroconf import IPVersion, ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf
 
 # Constants
 TYPE = "_adb-tls-pairing._tcp.local."
 NAME = "debug"
-PASSWORD = "123456"
+PASSWORD = randint(100000, 999999)
 FORMAT_QR = "WIFI:T:ADB;S:{name};P:{password};;"
 CMD_SHOW = "qrencode -t UTF8 '{data}'"
 CMD_PAIR = "adb pair {ip}:{port} {code}"
@@ -30,12 +32,13 @@ class ADBListener(ServiceListener):
         logging.debug(f"Service {name} removed.")
 
     def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        info = zc.get_service_info(type_, name)
+        info: ServiceInfo | None = zc.get_service_info(type_, name)
+        assert info, f"Service {name} not found."
         logging.debug(f"Service {name} added.")
         logging.debug(f"Service info: {info}")
         self.pair(info)
 
-    def pair(self, info) -> None:
+    def pair(self, info: ServiceInfo) -> None:
         cmd = CMD_PAIR.format(ip=info.server, port=info.port, code=PASSWORD)
         logging.debug(f"Executing command: {cmd}")
         process = subprocess.run(cmd, shell=True, capture_output=True)
@@ -47,7 +50,9 @@ class ADBListener(ServiceListener):
             exit(1)
 
         if stdout.startswith(SUCCESS_MSG):
-            print(SUCCESS_MSG + "!")
+            print(
+                f"{Fore.GREEN}Successfully paired with {Fore.LIGHTYELLOW_EX}{info.ip_addresses_by_version(IPVersion.All)[0].exploded}{Fore.WHITE}:{Fore.LIGHTYELLOW_EX}{info.port}{Fore.RESET}"
+            )
             exit(0)
 
     def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
@@ -55,6 +60,8 @@ class ADBListener(ServiceListener):
 
 
 def main() -> None:
+    just_fix_windows_console()
+
     logging.basicConfig(
         level=logging.DEBUG if len(argv) > 1 else logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
@@ -65,9 +72,9 @@ def main() -> None:
     qr.make(fit=True)
     qr.print_ascii(invert=True)
 
-    print("Scan QR code to pair new device.")
+    print(Fore.RED + "Scan QR-code to pair device." + Fore.RESET)
     print(
-        "[System]->[Developer options]->[Wireless debugging]->[Pair device with QR code]"
+        f"{Fore.CYAN}[System]{Fore.WHITE}->{Fore.CYAN}[Developer options]{Fore.WHITE}->{Fore.CYAN}[Wireless debugging]{Fore.WHITE}->{Fore.CYAN}[Pair device with QR code]{Fore.RESET}"
     )
 
     zeroconf = Zeroconf()
